ae2e0746688e46b45b55d51a02411d763209e9c8
George <91233711+Geor03@users.noreply.github.com>
2021-10-02 22:59:01 +0700

    gataui

--- NEW ADD FOOD/script.js
+++ NEW ADD FOOD/script.js
@@ -83,3 +83,26 @@ function onDelete(td) {
 //     }
 //     return isValid;
 // }
+// function shuffle(array) {
+//     let currentIndex = array.length,  randomIndex;
+  
+//     // While there remain elements to shuffle...
+//     while (currentIndex != 0) {
+  
+//       // Pick a remaining element...
+//       randomIndex = Math.floor(Math.random() * currentIndex);
+//       currentIndex--;
+  
+//       // And swap it with the current element.
+//       [array[currentIndex], array[randomIndex]] = [
+//         array[randomIndex], array[currentIndex]];
+//     }
+    
+//     return array;
+//   }
+var gaga = ['Martabak', 'GOreng', 'Tepung'];
+var ran = gaga.length;
+function random(item){
+var item = Math.floor(Math.random() * ran);
+  console.log(gaga[item]);
+}
