cf18c958e807ffca70ac1bcdb2f5187a303a32fb
Rofty <57068821+therofty911@users.noreply.github.com>
2021-10-04 19:54:57 +0700

    update alert

--- TODOFOOD/js/main.js
+++ TODOFOOD/js/main.js
@@ -17,6 +17,7 @@ addtaskbtn.addEventListener("click", function(){
 		// console.log(taskObj, 'Ashendra');
         localStorage.setItem("localtask", JSON.stringify(taskObj));
         addtaskinput.value = '';
+        alert("Your data has been added");
     }
     showtask();
 })
@@ -41,11 +42,14 @@ function showtask(){
         // }else{
             taskCompleteValue = `<td>${item.task_name}</td>`;
         //}
+        // var newRow = table.insertRow(table.length);
+        // cell2 = newRow.insertCell(1);
+        // cell2.innerHTML = `<thead><th>Name</th></thead>`;
         html += `<tbody>
                 <tr>
                     <th scope="row">${index+1}</th>
                     ${taskCompleteValue}
-                    <td><button class="btn btn-success me-lg-5 mb-1 me-2" type="button" onclick="edittask(${index})"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16" onClick="onEdit(this)"><path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/></svg>Edit</button></td>
+                    <td><button class="btn btn-success" type="button" onclick="edittask(${index})"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16" onClick="onEdit(this)"><path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/></svg>Edit</button></td>
                     <td><button class="btn btn-danger" type="button" onclick="deleteitem(${index})"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"><path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/></svg>Delete</button></td>
                 </tr></tbody>`;
     });
@@ -61,10 +65,15 @@ function edittask(index){
     saveindex.value = index;
     let webtask = localStorage.getItem("localtask");
     let taskObj = JSON.parse(webtask); 
-    
-    addtaskinput.value = taskObj[index]['task_name'];
-    addtaskbtn.style.display="none";
-    savetaskbtn.style.display="block";
+    let confirmEdit = confirm("Do you wanna edit this list?");
+    if (confirmEdit) {
+        addtaskinput.value = taskObj[index]['task_name'];
+        addtaskbtn.style.display="none";
+        savetaskbtn.style.display="block";
+        alert("You can edit the list food");
+    } else {
+        alert("You have been cancled for edit the list");
+    }
 }
 
 // savetask
@@ -77,9 +86,10 @@ savetaskbtn.addEventListener("click", function(){
     
     for (keys in taskObj[saveindex]) {
         if(keys == 'task_name'){
+            alert("Your data has been updated!");
             taskObj[saveindex].task_name = addtaskinput.value;
         }
-      }
+    }
     // taskObj[saveindex] = {'task_name':addtaskinput.value, 'completeStatus':false} ;
   //  taskObj[saveindex][task_name] = addtaskinput.value;
     savetaskbtn.style.display="none";
@@ -92,24 +102,16 @@ savetaskbtn.addEventListener("click", function(){
 function deleteitem(index){
     let webtask = localStorage.getItem("localtask");
     let taskObj = JSON.parse(webtask);
-    taskObj.splice(index, 1);
-    localStorage.setItem("localtask", JSON.stringify(taskObj));
+    let confirmDelete = confirm("Are you sure to delete this list?");
+    if (confirmDelete) {
+        taskObj.splice(index, 1);
+        localStorage.setItem("localtask", JSON.stringify(taskObj));
+      alert("Your list food has been deleted");
+    } else {
+      alert("Remove food has been cancled");
+    }
     showtask();
 }
-
-//complete task
-/* function completetask(index){
-    let webtask = localStorage.getItem("localtask");
-    let taskObj = JSON.parse(webtask);
-    taskObj[index] = '<span style="text-decoration:line-through">' + taskObj[index] + '</span>';
-    let addedtasklist = document.getElementById("addedtasklist");
-    addedtasklist.addEventListener("click", function(e){
-        console.log(addedtasklist)
-    })
-    localStorage.setItem("localtask", JSON.stringify(taskObj));
-    showtask();
-} */
-
 // complete task
 let addedtasklist = document.getElementById("addedtasklist");
     addedtasklist.addEventListener("click", function(e){
@@ -144,51 +146,7 @@ let addedtasklist = document.getElementById("addedtasklist");
         localStorage.setItem("localtask", JSON.stringify(taskObj));
         showtask();
     }
-    })
-
-    
-
-
-
-// deleteall
-let deleteallbtn = document.getElementById("deleteallbtn");
-deleteallbtn.addEventListener("click", function(){
-    let savetaskbtn = document.getElementById("savetaskbtn");
-    let addtaskbtn = document.getElementById("addtaskbtn");
-    let webtask = localStorage.getItem("localtask");
-    let taskObj = JSON.parse(webtask);
-    if(webtask == null){
-        taskObj = [];
-    }
-    else{
-        taskObj = JSON.parse(webtask);
-        taskObj = [];
-    }
-    savetaskbtn.style.display="none";
-    addtaskbtn.style.display="block";
-    localStorage.setItem("localtask", JSON.stringify(taskObj));
-    showtask();
-
 })
-
-
-// serachlist
-let searchtextbox = document.getElementById("searchtextbox");
-searchtextbox.addEventListener("input", function(){
-    let trlist = document.querySelectorAll("tr");
-    Array.from(trlist).forEach(function(item){
-        let searchedtext = item.getElementsByTagName("td")[0].innerText;
-        let searchtextboxval = searchtextbox.value;
-        let re = new RegExp(searchtextboxval, 'gi');
-        if(searchedtext.match(re)){
-            item.style.display="table-row";
-        }
-        else{
-            item.style.display="none";
-        }
-    })
-})
-
 
 function random(item){
     let webtask = localStorage.getItem("localtask");
@@ -196,6 +154,7 @@ function random(item){
     let tempcon = taskObj;
     let ran = tempcon.length;
     let test = [];
+    // let validTable = document.getElementById("tableMenuBod");
     if(webtask == null){
         console.log("list is empty");
         return false;
@@ -205,7 +164,7 @@ function random(item){
             for(let i = 0; i<3; i++){
                 var item = Math.floor(Math.random() * ran);
                 test.push(tempcon[item]);
-                console.log(test[0], test[1], test[2]);
+                // console.log(test[0], test[1], test[2]);
             }
             addtorandom(test);
         }
@@ -213,7 +172,7 @@ function random(item){
             for(let i = 0; i<3; i++){
                 var item = Math.floor(Math.random() * (ran));
                 test.push(tempcon[item]);
-                console.log(item, test[0], test[1], test[2]);
+                // console.log(item, test[0], test[1], test[2]);
                 ran--;
                 tempcon.splice(item,1);
             }
