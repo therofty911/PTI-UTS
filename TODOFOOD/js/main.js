e165988af26ee45891a61bf60833dee88e6d5083
Rofty <57068821+therofty911@users.noreply.github.com>
2021-10-05 03:43:49 +0700

    add delete all and fix responsive

--- TODOFOOD/js/main.js
+++ TODOFOOD/js/main.js
@@ -33,7 +33,6 @@ function showfood(){
         foodObj = JSON.parse(webfood);
     }
     let html = '';
-    let head = '';
     
     let addedfoodlist = document.getElementById("addedfoodlist");
     foodObj.forEach((item, index) => {
@@ -42,13 +41,16 @@ function showfood(){
         // var newRow = table.insertRow(table.length);
         // cell2 = newRow.insertCell(1);
         // cell2.innerHTML = `<thead><th>Name</th></thead>`;
-        html += `<tbody>
+        
+        html += `
+        <tbody class="text-center" id="tableMenuBod">
                 <tr>
-                    <th scope="row">${index+1}</th>
+                    <td scope="row">${index+1}</td>
                     ${foodCompleteValue}
                     <td><button class="btn btn-success" type="button" onclick="editfood(${index})"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16" onClick="onEdit(this)"><path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/></svg>Edit</button></td>
                     <td><button class="btn btn-danger" type="button" onclick="deleteitem(${index})"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"><path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/></svg>Delete</button></td>
                 </tr></tbody>`;
+                
     });
     addedfoodlist.innerHTML = html;
 
@@ -107,6 +109,34 @@ function deleteitem(index){
     }
     showfood();
 }
+
+// deleteall
+let deleteallbtn = document.getElementById("deleteallbtn");
+deleteallbtn.addEventListener("click", function(){
+    let savefoodbtn = document.getElementById("savefoodbtn");
+    let addfoodbtn = document.getElementById("addfoodbtn");
+    let webfood = localStorage.getItem("localfood");
+    let foodObj = JSON.parse(webfood);
+    let confirmDelete = confirm("Are you sure to delete all this list?");
+    if (confirmDelete) {
+        if(webfood == null){
+            foodObj = [];
+        }
+        else{
+            foodObj = JSON.parse(webfood);
+            foodObj = [];
+        }
+    } else {
+      alert("Remove food has been cancled");
+    }
+    
+    savefoodbtn.style.display="none";
+    addfoodbtn.style.display="block";
+    localStorage.setItem("localfood", JSON.stringify(foodObj));
+    showfood();
+
+})
+
 // complete food
 let addedfoodlist = document.getElementById("addedfoodlist");
     addedfoodlist.addEventListener("click", function(e){
